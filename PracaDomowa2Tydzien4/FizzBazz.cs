3e8ee9665fa0e660c3f692f43833136322f9a303
Pawel <you@example.com>
2020-07-27 22:02:17 +0200

    adding class FizzBazz

---
+++ PracaDomowa2Tydzien4/FizzBazz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracaDomowa2Tydzien4
+{
+   public class FizzBazz
+    {
+        
+
+    }
+}
